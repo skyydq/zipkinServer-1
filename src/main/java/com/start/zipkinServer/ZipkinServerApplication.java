02d5b0efc206ebc0669b7fca2c472bc776ae90dd
junhui <junhhe@cisco.com>
2020-11-24 18:19:18 +0800

    update

---
+++ src/main/java/com/start/zipkinServer/ZipkinServerApplication.java
@@ -0,0 +1,18 @@
+package com.start.zipkinServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableDiscoveryClient
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
